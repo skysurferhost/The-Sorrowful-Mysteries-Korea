1a5903428ea87746d915b3cb0922db9c5031942c
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-03-12 23:46:00 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-lo-bun--m-hi-mu",
+      "name": "1. 예수님께서 겟세마니에서 기도하심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8763970383256172,
+          "pitch": 0.16735844249532406,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-chu-nh-n"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8315975342972379,
+          "pitch": -0.1932127154048935,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">겟세마니에서 기도하시다</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">그때에 예수님께서 제자들과 함께 겟세마니라는 곳으로 가셨다. 그리고 제자들에게, “내가 저기 가서 기도하는 동안 여기에 앉아 있어라.” 하고 말씀하신 다음, 베드로와 제베대오의 두 아들을 데리고 가셨다. 그분께서는 근심과 번민에 휩싸이기 시작하셨다. 그때에 그들에게 “내 마음이 너무 괴로워 죽을 지경이다. 너희는 여기에 남아서 나와 함께 깨어 있어라.” 하고 말씀하셨다. 그런 다음 앞으로 조금 나아가 얼굴을 땅에 대고 기도하시며 이렇게 말씀하셨다. “아버지, 하실 수만 있으시면 이 잔이 저를 비켜 가게 해 주십시오. 그러나 제가 원하는 대로 하지 마시고 아버지께서 원하시는 대로 하십시오.” 그러고 나서 제자들에게 돌아와 보시니 그들은 자고 있었다. 그래서 베드로에게 “이렇게 너희는 나와 함께 한 시간도 깨어 있을 수 없더란 말이냐? 유혹에 빠지지 않도록 깨어 기도하여라. 마음은 간절하나 몸이 따르지 못한다.” 하시고, 다시 두 번째로 가서 기도하셨다. “아버지, 이 잔이 비켜 갈 수 없는 것이라서 제가 마셔야 한다면, 아버지의 뜻이 이루어지게 하십시오.” 그리고 다시 와 보시니 그들은 여전히 눈이 무겁게 감겨 자고 있었다. 예수님께서는 그들을 그대로 두시고 다시 가시어 세 번째 같은 말씀으로 기도하셨다. 그리고 제자들에게 돌아와 말씀하셨다. “아직도 자고 있느냐? 아직도 쉬고 있느냐? 이제 때가 가까웠다. 사람의 아들은 죄인들의 손에 넘어간다. 일어나 가자. 보라, 나를 팔아넘길 자가 가까이 왔다.”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(마태 26:36-46)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-chu-nh-n",
+      "name": "2. 예수님께서 채찍질을 받으심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03994914720259146,
+          "pitch": -0.13235010662935842,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-chu-i-mo-gai"
+        },
+        {
+          "yaw": -2.3599925639905557,
+          "pitch": -0.09310296493766934,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-lo-bun--m-hi-mu"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0021049083788469147,
+          "pitch": -0.4432680786781624,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">빌라도에게 신문을 받으시다</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">아침이 되자 수석 사제들은 곧바로 원로들과 율법 학자들, 곧 온 최고 의회와 의논한 끝에, 예수님을 결박하여 끌고 가서 빌라도에게 넘겼다.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">빌라도가 예수님께 “당신이 유다인들의 임금이오?” 하고 묻자, 그분께서 “네가 그렇게 말하고 있다.” 하고 대답하셨다. 그러자 수석 사제들이 여러 가지로 예수님을 고소하였다. 빌라도가 다시 예수님께, “당신은 아무 대답도 하지 않소? 보시오, 저들이 당신을 갖가지로 고소하고 있지 않소?” 하고 물었으나, 예수님께서는 더 이상 아무 대답도 하지 않으셨다. 그래서 빌라도는 이상하게 여겼다.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">빌라도는 축제 때마다 사람들이 요구하는 죄수 하나를 풀어 주곤 하였다. 마침 바라빠라고 하는 사람이 반란 때에 살인을 저지른 반란군들과 함께 감옥에 있었다. 그래서 군중은 올라가 자기들에게 해 오던 대로 해 달라고 요청하기 시작하였다. 빌라도가 그들에게 “유다인들의 임금을 풀어 주기를 바라는 것이오?” 하고 물었다. 그는 수석 사제들이 예수님을 시기하여 자기에게 넘겼음을 알고 있었던 것이다. 그러나 수석 사제들은 군중을 부추겨 그분이 아니라 바라빠를 풀어 달라고 청하게 하였다. 빌라도가 다시 그들에게, “그러면 여러분이 유다인들의 임금이라고 부르는 이 사람은 어떻게 하기를 바라는 것이오?” 하고 물었다. 그러자 그들은 “십자가에 못 박으시오!” 하고 거듭 소리 질렀다. 빌라도가 그들에게 “도대체 그가 무슨 나쁜 짓을 하였다는 말이오?” 하자, 그들은 더욱 큰 소리로 “십자가에 못 박으시오!” 하고 외쳤다. 그리하여 빌라도는 군중을 만족시키려고, 바라빠를 풀어 주고 예수님을 채찍질하게 한 다음 십자가에 못 박으라고 넘겨주었다.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(마르 15:1-15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-chu-i-mo-gai",
+      "name": "3. 예수님께서 가시관을 쓰심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.770374617292628,
+          "pitch": -0.011154715826149797,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-vc-cy-thnh-gi"
+        },
+        {
+          "yaw": -3.1385039082777517,
+          "pitch": -0.14273786458115012,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-chu-nh-n"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7943040273107442,
+          "pitch": -0.21542148670605954,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">군사들이 예수님을 조롱하다</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">군사들은 예수님을 뜰 안으로 끌고 갔다. 그곳은 총독 관저였다. 그들은 온 부대를 집합시킨 다음, 그분께 자주색 옷을 입히고 가시관을 엮어 머리에 씌우고서는, “유다인들의 임금님, 만세!” 하며 인사하기 시작하였다. 또 갈대로 그분의 머리를 때리고 침을 뱉고서는, 무릎을 꿇고 엎드려 예수님께 절하였다. 그렇게 예수님을 조롱하고 나서 자주색 옷을 벗기고 그분의 겉옷을 입혔다. 그리고 예수님을 십자가에 못 박으러 끌고 나갔다.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(마르 15:16-20)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-vc-cy-thnh-gi",
+      "name": "4. 예수님께서 십자가를 지고 가심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6983594634984662,
+          "pitch": -0.37386622268201464,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-chu-cht-trn-cy-thnh-gi"
+        },
+        {
+          "yaw": 3.1374699261000316,
+          "pitch": 0.2915448346724503,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-chu-i-mo-gai"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.66061327614063,
+          "pitch": -0.6363281998126844,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">예수님께서 십자가를 지고 가심</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">그들은 예수님을 끌고 가다가, 시골에서 오고 있던 시몬이라는 어떤 키레네 사람을 붙잡아 십자가를 지우고 예수님을 뒤따르게 하였다. 백성의 큰 무리도 예수님을 따라갔다. 그 가운데에는 예수님 때문에 가슴을 치며 통곡하는 여자들도 있었다. 예수님께서는 그 여자들에게 돌아서서 이르셨다. “예루살렘의 딸들아, 나 때문에 울지 말고 너희와 너희 자녀들 때문에 울어라. 보라, ‘아이를 낳지 못하는 여자, 아이를 배어 보지 못하고 젖을 먹여 보지 못한 여자는 행복하여라!’ 하고 말할 날이 올 것이다. 그때에 사람들은 ‘산들에게 ′우리 위로 무너져 내려라.′ 하고 언덕들에게 ′우리를 덮어 다오.′ 할’ 것이다. 푸른 나무가 이러한 일을 당하거든 마른나무야 어떻게 되겠느냐?”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(루카 23:26-31)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-chu-cht-trn-cy-thnh-gi",
+      "name": "5. 예수님께서 십자가에 못 박혀 돌아가심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7763165764238629,
+          "pitch": 0.39931217659419715,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-vc-cy-thnh-gi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4895233345042449,
+          "pitch": -0.6379339964485311,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">십자가에 못 박히시다</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">그들은 예수님을 넘겨받았다.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">예수님께서는 몸소 십자가를 지시고 ‘해골 터’라는 곳으로 나가셨다. 그곳은 히브리 말로 골고타라고 한다. 거기에서 그들은 예수님을 십자가에 못 박았다. 그리고 다른 두 사람도 예수님을 가운데로 하여 이쪽저쪽에 하나씩 못 박았다. 빌라도는 명패를 써서 십자가 위에 달게 하였는데, 거기에는 ‘유다인들의 임금 나자렛 사람 예수’라고 쓰여 있었다. 예수님께서 십자가에 못 박히신 곳이 도성에서 가까웠기 때문에, 많은 유다인이 그 명패를 읽게 되었다. 그것은 히브리 말, 라틴 말, 그리스 말로 쓰여 있었다. 그래서 유다인들의 수석 사제들이 빌라도에게 말하였다. “‘유다인들의 임금’이라고 쓸 것이 아니라, ‘나는 유다인들의 임금이다.’ 하고 저자가 말하였다고 쓰시오.” 그러나 빌라도는 “내가 한번 썼으면 그만이오.” 하고 대답하였다.</b></div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">군사들은 예수님을 십자가에 못 박고 나서, 그분의 옷을 가져다가 네 몫으로 나누어 저마다 한몫씩 차지하였다. 속옷도 가져갔는데 그것은 솔기가 없이 위에서부터 통으로 짠 것이었다. 그래서 그들은 서로, “이것은 찢지 말고 누구 차지가 될지 제비를 뽑자.” 하고 말하였다. “그들이 제 옷을 저희끼리 나누어 가지고 제 속옷을 놓고서는 제비를 뽑았습니다.” 하신 성경 말씀이 이루어지려고 그리된 것이다. 그래서 군사들이 그렇게 하였다.</b></div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">예수님의 십자가 곁에는 그분의 어머니와 이모, 클로파스의 아내 마리아와 마리아 막달레나가 서 있었다. 예수님께서는 당신의 어머니와 그 곁에 선 사랑하시는 제자를 보시고, 어머니에게 말씀하셨다. “여인이시여, 이 사람이 어머니의 아들입니다.” 이어서 그 제자에게 “이분이 네 어머니시다.” 하고 말씀하셨다. 그때부터 그 제자가 그분을 자기 집에 모셨다.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">그 뒤에 이미 모든 일이 다 이루어졌음을 아신 예수님께서는 성경 말씀이 이루어지게 하시려고 “목마르다.” 하고 말씀하셨다. 거기에는 신 포도주가 가득 담긴 그릇이 놓여 있었다. 그래서 사람들이 신 포도주를 듬뿍 적신 해면을 우슬초 가지에 꽂아 예수님의 입에 갖다 대었다. 예수님께서는 신 포도주를 드신 다음에 말씀하셨다. “다 이루어졌다.” 이어서 고개를 숙이시며 숨을 거두셨다.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(요한 19:16-30)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
